2b07a398c8bbbb6a64fff7a81610f138cd0d8522
TigorLazuard <tigor.hutasuhut@gmail.com>
2020-07-18 19:37:47 +0700

    up

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
                 <p>
-                    Cabs <i>NGENS</i> Kids
+                    Evans kerjaannya Cabs <i>NGENS</i> Kids
                 </p>
                 <a
                     className="App-link"
